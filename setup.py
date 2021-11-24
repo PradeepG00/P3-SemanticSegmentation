2744aa962af3ccedce831dfed40f137dc37286cd
Henry Lao <hclao1995@gmail.com>
2021-11-24 05:29:34 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# from setuptools import setup
+
+# setup.py
+# from setuptools import setup, find_packages
+import setuptools
+
+
+setuptools.setup(
+    # defined as the cli package (Note the naming)
+    name='mscg-net',
+    version='0.1.0',
+    author="",
+    author_email="",
+    url="",
+    # functions as a search for the name of the package defined in the name variable
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+    ''',
+    python_requires=">=3.6"
+)
