b9a66890208fdc66f15b784f166206968ac0f9b7
Henry Lao <hclao1995@gmail.com>
2021-12-01 05:42:56 -0800

    Update documentation of data access

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,15 +3,15 @@ import os.path
 from pathlib import Path
 
 PROJECT_ROOT = Path(os.path.abspath(__file__)).parent.parent
-
+CHECKPOINT_DIR = PROJECT_ROOT / "checkpoints"
 # toggle for less verbose output
-DEBUG = True
+DEBUG = False
 
 
-def check_mkdir(dir_name) -> None:
+def check_mkdir(dir_name: str) -> None:
     """Utility function that creates a directory if the path does not exist
 
-    :param dir_name:
+    :param dir_name: str
     :return:
     """
     if not os.path.exists(dir_name):
